313a345dc0e8be2af8ecdfc5d1da66b1ec890fb6
wy88militia-code <wy88militia@gmail.com>
2026-09-06 10:23:05 +0900

    Allow Finance access to Partner Deposit receipt UAT

--- netlify/functions/_admin-rbac-core.mjs
+++ netlify/functions/_admin-rbac-core.mjs
@@ -16,6 +16,7 @@ const PATH_ROLES=[
  [/^\/admin-phase1-accurate-si-production-readiness$/,['SUPERADMIN','FINANCE']],
  [/^\/admin-phase1-partner-deposit-bridge$/,['SUPERADMIN','FINANCE']],
  [/^\/admin-phase1-partner-deposit-dp-uat$/,['SUPERADMIN','FINANCE']],
+ [/^\/admin-phase1-partner-deposit-receipt-uat$/,['SUPERADMIN','FINANCE']],
  [/^\/admin-accurate(?:\/simulation)?$/,['SUPERADMIN','FINANCE']],
  [/^\/admin-quotes$/,['SUPERADMIN','FINANCE','OPS']],
  [/^\/admin-master-sheet$/,['SUPERADMIN','OPS']],
